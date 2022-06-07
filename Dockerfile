5e6ea53468bb40cfaed7e8dee2a8d62107a69795
Riccardo De Leo <r@rdeleo.com>
2022-06-07 19:01:52 +0200

    GET-1465 | test(codebase): installed and configured phpunit to run and generate code coverage

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,11 @@ RUN apk add --no-cache \
       zip \
     && docker-php-ext-install zip
 
+RUN apk --no-cache add pcre-dev ${PHPIZE_DEPS} \
+  && pecl install xdebug \
+  && docker-php-ext-enable xdebug \
+  && apk del pcre-dev ${PHPIZE_DEPS}
+
 # Install composer
 COPY --from=composer/composer:2 /usr/bin/composer /usr/bin/composer
 RUN chmod +x /usr/bin/composer
---
+++ tests/Unit/ClientTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace WeGetFinancing\PHPSDK\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use WeGetFinancing\PHPSDK\Client;
+
+final class ClientTest extends TestCase
+{
+    private Client $sut;
+
+    public function setUp(): void
+    {
+        $this->sut = new Client();
+    }
+
+    public function testClientIsInstantiatedAndUnitTestAreRunning(): void
+    {
+        $this->assertInstanceOf(Client::class, $this->sut);
+    }
+}
