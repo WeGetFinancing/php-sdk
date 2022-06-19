fe9dcec08e24622957e5498a8f0e6ce3e09482e9
Riccardo De Leo <r@rdeleo.com>
2022-06-20 01:32:36 +0200

    GET-1465 | chore(tests): test entity refactoring in preparation of loan request entity test

--- tests/functional/Entity/Request/AddressEntityTest.php
+++ tests/functional/Entity/Request/AddressEntityTest.php
@@ -13,6 +13,176 @@ use PHPUnit\Framework\TestCase;
  */
 final class AddressEntityTest extends TestCase
 {
+    public const VALID_ITEM_1 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607'
+        ],
+        'expected' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607'
+        ]
+    ];
+
+    public const VALID_ITEM_2 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607'
+        ],
+        'expected' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607'
+        ]
+    ];
+
+    public const INVALID_ITEM_1 = [
+        'entity' => [
+            'street1' => '',
+            'city' => '',
+            'state' => '',
+            'zipcode' => '',
+        ],
+        'violations' => [
+            'The value of street1 is too short. It should have 2 characters or more.',
+            'The value of street1 should not be blank.',
+            'The value of city is too short. It should have 2 characters or more.',
+            'The value of city should not be blank.',
+            'The value of state should have exactly 2 characters.',
+            'The value of state should not be blank.',
+            'The value of zipcode should not be blank.',
+        ]
+    ];
+
+    public const INVALID_ITEM_2 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'Redmond',
+            'state' => 'WAA',
+            'zipcode' => '666666',
+        ],
+        'violations' => [
+            'The value of state should have exactly 2 characters.',
+            'The value of zipcode should contain only 5 numbers optionally followed by a dash and 4 numbers.',
+        ]
+    ];
+
+    public const INVALID_ITEM_3 = [
+        'entity' => [
+            'street1' => '1',
+            'city' => 'R',
+            'state' => 'W',
+            'zipcode' => '55555-55555',
+        ],
+        'violations' => [
+            'The value of street1 is too short. It should have 2 characters or more.',
+            'The value of city is too short. It should have 2 characters or more.',
+            'The value of state should have exactly 2 characters.',
+            'The value of zipcode should contain only 5 numbers optionally followed by a dash and 4 numbers.',
+        ]
+    ];
+
+    public const INVALID_ITEM_4 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+        ],
+        'violations' => [
+            'The value of state should not be blank.',
+            'The value of zipcode should not be blank.',
+        ]
+    ];
+
+    public const INVALID_ITEM_5 = [
+        'entity' => [
+            'state' => 'NY',
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'The value of street1 should not be blank.',
+            'The value of city should not be blank.',
+        ]
+    ];
+
+    public const INVALID_ITEM_6 = [
+        'entity' => [
+            'street1' => null,
+            'city' => 7.9,
+            'state' => 'NY',
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'Cannot assign null to property App\Entity\Request\AddressEntity::$street1 of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_7 = [
+        'entity' => [
+            'street1' => 7.5,
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'Cannot assign float to property App\Entity\Request\AddressEntity::$street1 of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_8 = [
+        'entity' => [
+            'street1' => 7.5,
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'Cannot assign float to property App\Entity\Request\AddressEntity::$street1 of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_9 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => null,
+            'state' => 'NY',
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'Cannot assign null to property App\Entity\Request\AddressEntity::$city of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_10 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => null,
+            'zipcode' => '66607',
+        ],
+        'violations' => [
+            'Cannot assign null to property App\Entity\Request\AddressEntity::$state of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_11 = [
+        'entity' => [
+            'street1' => '15th Fantasy Street',
+            'city' => 'New York',
+            'state' => 'NY',
+            'zipcode' => 1,
+        ],
+        'violations' => [
+            'Cannot assign int to property App\Entity\Request\AddressEntity::$zipcode of type string',
+        ]
+    ];
+
     protected AddressEntity $sut;
 
     /**
@@ -25,22 +195,12 @@ final class AddressEntityTest extends TestCase
     }
 
     /**
-     * @return iterable<array<array<string, string>>>
+     * @return iterable<array<array<string, array<string, string>>>>
      */
     public function getValidAddressEntityData(): iterable
     {
-        yield [[
-            "street1" => "15th Fantasy Street",
-            "city" => "New York",
-            "state" => "NY",
-            "zipcode" => "66607",
-        ]];
-        yield [[
-            "street1" => "15th Fantasy Street",
-            "city" => "Redmond",
-            "state" => "WA",
-            "zipcode" => "55555-4444",
-        ]];
+        yield [ self::VALID_ITEM_1 ];
+        yield [ self::VALID_ITEM_2 ];
     }
 
     /**
@@ -54,144 +214,29 @@ final class AddressEntityTest extends TestCase
      */
     public function testMakeWithDataWillSucceedAsExpected(array $data): void
     {
-        $this->sut = AddressEntity::make($data);
-
-        $this->assertSame($data["street1"], $this->sut->street1);
-        $this->assertSame($data["city"], $this->sut->city);
-        $this->assertSame($data["state"], $this->sut->state);
-        $this->assertSame($data["zipcode"], $this->sut->zipcode);
-
+        $this->sut = AddressEntity::make($data['entity']);
         $this->assertSame(
-            $data,
+            $data['expected'],
             $this->sut->getWeGetFinancingRequest()
         );
     }
 
     /**
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     *
      * @return iterable<int, array<array<string, array<int|string, mixed>>>>
      */
     public function getInvalidAddressEntityData(): iterable
     {
-        yield [[
-            'entity' => [
-                'street1' => '',
-                'city' => '',
-                'state' => '',
-                'zipcode' => '',
-            ],
-            'violations' => [
-                'The value of street1 is too short. It should have 2 characters or more.',
-                'The value of street1 should not be blank.',
-                'The value of city is too short. It should have 2 characters or more.',
-                'The value of city should not be blank.',
-                'The value of state should have exactly 2 characters.',
-                'The value of state should not be blank.',
-                'The value of zipcode should not be blank.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '15th Fantasy Street',
-                'city' => 'Redmond',
-                'state' => 'WAA',
-                'zipcode' => '666666',
-            ],
-            'violations' => [
-                'The value of state should have exactly 2 characters.',
-                'The value of zipcode should contain only 5 numbers optionally followed by a dash and 4 numbers.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '1',
-                'city' => 'R',
-                'state' => 'W',
-                'zipcode' => '55555-55555',
-            ],
-            'violations' => [
-                'The value of street1 is too short. It should have 2 characters or more.',
-                'The value of city is too short. It should have 2 characters or more.',
-                'The value of state should have exactly 2 characters.',
-                'The value of zipcode should contain only 5 numbers optionally followed by a dash and 4 numbers.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '15th Fantasy Street',
-                'city' => 'New York',
-            ],
-            'violations' => [
-                'The value of state should not be blank.',
-                'The value of zipcode should not be blank.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'state' => 'NY',
-                'zipcode' => '66607',
-            ],
-            'violations' => [
-                'The value of street1 should not be blank.',
-                'The value of city should not be blank.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => null,
-                'city' => 7.9,
-                'state' => 'NY',
-                'zipcode' => '66607',
-            ],
-            'violations' => [
-                'Cannot assign null to property App\Entity\Request\AddressEntity::$street1 of type string',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => 7.5,
-                'city' => 'New York',
-                'state' => 'NY',
-                'zipcode' => '66607',
-            ],
-            'violations' => [
-                'Cannot assign float to property App\Entity\Request\AddressEntity::$street1 of type string',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '15th Fantasy Street',
-                'city' => null,
-                'state' => 'NY',
-                'zipcode' => '66607',
-            ],
-            'violations' => [
-                'Cannot assign null to property App\Entity\Request\AddressEntity::$city of type string',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '15th Fantasy Street',
-                'city' => 'New York',
-                'state' => null,
-                'zipcode' => '66607',
-            ],
-            'violations' => [
-                'Cannot assign null to property App\Entity\Request\AddressEntity::$state of type string',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'street1' => '15th Fantasy Street',
-                'city' => 'New York',
-                'state' => 'NY',
-                'zipcode' => 1,
-            ],
-            'violations' => [
-                'Cannot assign int to property App\Entity\Request\AddressEntity::$zipcode of type string',
-            ],
-        ]];
+        yield [ self::INVALID_ITEM_1 ];
+        yield [ self::INVALID_ITEM_2 ];
+        yield [ self::INVALID_ITEM_3 ];
+        yield [ self::INVALID_ITEM_4 ];
+        yield [ self::INVALID_ITEM_5 ];
+        yield [ self::INVALID_ITEM_6 ];
+        yield [ self::INVALID_ITEM_7 ];
+        yield [ self::INVALID_ITEM_8 ];
+        yield [ self::INVALID_ITEM_9 ];
+        yield [ self::INVALID_ITEM_10 ];
+        yield [ self::INVALID_ITEM_11 ];
     }
 
     /**
@@ -205,9 +250,9 @@ final class AddressEntityTest extends TestCase
     public function testMakeWithDataWillFailAsExpected(array $data): void
     {
         try {
-            AddressEntity::make($data["entity"]);
+            AddressEntity::make($data['entity']);
         } catch (EntityValidationException $exception) {
-            $this->assertSame($data["violations"], $exception->getViolations());
+            $this->assertSame($data['violations'], $exception->getViolations());
         }
     }
 }
--- tests/functional/Entity/Request/CartItemEntityTest.php
+++ tests/functional/Entity/Request/CartItemEntityTest.php
@@ -13,6 +13,116 @@ use PHPUnit\Framework\TestCase;
  */
 final class CartItemEntityTest extends TestCase
 {
+    public const VALID_ITEM_1 = [
+        'entity' => [
+            'sku' => 'SKU_CODE_001',
+            'displayName' => 'Cart product 001',
+            'unitPrice' => '10.0',
+            'quantity' => 1,
+            'unitTax' => 21.0,
+            'category' => 'CAT_A',
+        ],
+        'expected' => [
+            'sku' => 'SKU_CODE_001',
+            'display_name' => 'Cart product 001',
+            'unit_price' => '10.00',
+            'quantity' => 1,
+            'unit_tax' => '21.00',
+            'category' => 'CAT_A',
+        ]
+    ];
+
+    public const VALID_ITEM_2 = [
+        'entity' => [
+            'sku' => '02',
+            'displayName' => 'Cart product 002',
+            'unitPrice' => 256,
+            'quantity' => 6,
+            'unitTax' => 0.75,
+            'category' => 'CAT_A',
+        ],
+        'expected' => [
+            'sku' => '02',
+            'display_name' => 'Cart product 002',
+            'unit_price' => '256.00',
+            'quantity' => 6,
+            'unit_tax' => '0.75',
+            'category' => 'CAT_A',
+        ]
+    ];
+
+    public const VALID_ITEM_3 = [
+        'entity' => [
+            'sku' => 'ZeroThree',
+            'displayName' => 'Cart product zero zero three',
+            'unitPrice' => 8.86432,
+            'quantity' => 9,
+            'unitTax' => 134.6,
+        ],
+        'expected' => [
+            'sku' => 'ZeroThree',
+            'display_name' => 'Cart product zero zero three',
+            'unit_price' => '8.86',
+            'quantity' => 9,
+            'unit_tax' => '134.60',
+        ]
+    ];
+
+    public const INVALID_ITEM_1 = [
+        'entity' => [
+            'sku' => '',
+            'displayName' => '1',
+            'unitPrice' => '10.0',
+            'quantity' => -2,
+            'unitTax' => 0.0,
+        ],
+        'violations' => [
+            'The value of sku should not be blank.',
+            'The value of display name  is too short. It should have 2 characters or more.',
+            'The value of quantity should be positive.',
+        ]
+    ];
+
+    public const INVALID_ITEM_2 = [
+        'entity' => [
+            'sku' => '',
+            'displayName' => '1',
+            'unitPrice' => '0',
+            'quantity' => -2,
+            'unitTax' => 0.0,
+        ],
+        'violations' => [
+            'Unit Price value should not be equal or less than zero.',
+        ]
+    ];
+
+    public const INVALID_ITEM_3 = [
+        'entity' => [
+            'sku' => false,
+            'displayName' => '1',
+            'unitPrice' => '10',
+            'quantity' => -2,
+            'unitTax' => null,
+        ],
+        'violations' => [
+            'Cannot assign bool to property App\Entity\Request\CartItemEntity::$sku of type string',
+        ]
+    ];
+
+    public const INVALID_ITEM_4 = [
+        'entity' => [
+            'sku' => 'correct',
+            'displayName' => '1',
+            'unitPrice' => '10',
+            'quantity' => -2,
+            'unitTax' => null,
+        ],
+        'violations' => [
+            'Unit Tax Cannot assign null to property App\Entity\MoneyEntity::$value of type string',
+        ]
+    ];
+
+
     protected CartItemEntity $sut;
 
     /**
@@ -29,58 +139,9 @@ final class CartItemEntityTest extends TestCase
      */
     public function getValidCartItemEntityData(): iterable
     {
-        yield [[
-            "entity" => [
-                "sku" => "SKU_CODE_001",
-                "displayName" => "Cart product 001",
-                "unitPrice" => "10.0",
-                "quantity" => 1,
-                "unitTax" => 21.0,
-                "category" => "CAT_A",
-            ],
-            "expected" => [
-                "sku" => "SKU_CODE_001",
-                "display_name" => "Cart product 001",
-                "unit_price" => "10.00",
-                "quantity" => 1,
-                "unit_tax" => "21.00",
-                "category" => "CAT_A",
-            ],
-        ]];
-        yield [[
-            "entity" => [
-                "sku" => "02",
-                "displayName" => "Cart product 002",
-                "unitPrice" => 256,
-                "quantity" => 6,
-                "unitTax" => 0.75,
-                "category" => "CAT_A",
-            ],
-            "expected" => [
-                "sku" => "02",
-                "display_name" => "Cart product 002",
-                "unit_price" => "256.00",
-                "quantity" => 6,
-                "unit_tax" => "0.75",
-                "category" => "CAT_A",
-            ],
-        ]];
-        yield [[
-            "entity" => [
-                "sku" => "ZeroThree",
-                "displayName" => "Cart product zero zero three",
-                "unitPrice" => 8.86432,
-                "quantity" => 9,
-                "unitTax" => 134.6,
-            ],
-            "expected" => [
-                "sku" => "ZeroThree",
-                "display_name" => "Cart product zero zero three",
-                "unit_price" => "8.86",
-                "quantity" => 9,
-                "unit_tax" => "134.60",
-            ],
-        ]];
+        yield [ self::VALID_ITEM_1 ];
+        yield [ self::VALID_ITEM_2 ];
+        yield [ self::VALID_ITEM_3 ];
     }
 
     /**
@@ -103,56 +164,10 @@ final class CartItemEntityTest extends TestCase
      */
     public function getInvalidCartItemEntityData(): iterable
     {
-        yield [[
-            'entity' => [
-                'sku' => '',
-                'displayName' => '1',
-                'unitPrice' => '10.0',
-                'quantity' => -2,
-                'unitTax' => 0.0,
-            ],
-            'violations' => [
-                'The value of sku should not be blank.',
-                'The value of display name  is too short. It should have 2 characters or more.',
-                'The value of quantity should be positive.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'sku' => '',
-                'displayName' => '1',
-                'unitPrice' => '0',
-                'quantity' => -2,
-                'unitTax' => 0.0,
-            ],
-            'violations' => [
-                'Unit Price value should not be equal or less than zero.',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'sku' => false,
-                'displayName' => '1',
-                'unitPrice' => '10',
-                'quantity' => -2,
-                'unitTax' => null,
-            ],
-            'violations' => [
-                'Cannot assign bool to property App\Entity\Request\CartItemEntity::$sku of type string',
-            ],
-        ]];
-        yield [[
-            'entity' => [
-                'sku' => "correct",
-                'displayName' => '1',
-                'unitPrice' => '10',
-                'quantity' => -2,
-                'unitTax' => null,
-            ],
-            'violations' => [
-                'Unit Tax Cannot assign null to property App\Entity\MoneyEntity::$value of type string',
-            ],
-        ]];
+        yield [ self::INVALID_ITEM_1 ];
+        yield [ self::INVALID_ITEM_2 ];
+        yield [ self::INVALID_ITEM_3 ];
+        yield [ self::INVALID_ITEM_4 ];
     }
 
     /**
@@ -166,9 +181,9 @@ final class CartItemEntityTest extends TestCase
     public function testMakeWithDataWillFailAsExpected(array $data): void
     {
         try {
-            CartItemEntity::make($data["entity"]);
+            CartItemEntity::make($data['entity']);
         } catch (EntityValidationException $exception) {
-            $this->assertSame($data["violations"], $exception->getViolations());
+            $this->assertSame($data['violations'], $exception->getViolations());
         }
     }
 }
